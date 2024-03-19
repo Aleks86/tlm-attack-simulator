e0e0dfcccc0a990986a5196658e2a96d5436825e
Aleks <aleksandar.babic@2amigos.us>
2024-03-19 22:33:00 +0100

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  let base = "/";
+  if (mode === "production") {
+    base = "/tlm-attack-simulator/";
+  }
+  return {
+    plugins: [react()],
+    base,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+  };
+});
